b6f830468b4ebba21dc63522abe23ccf1c5083e1
James <wong.james2003@gmail.com>
2024-10-13 22:14:17 -0400

    fixing everything

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, StatusMessage
 
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'city', 'email')
+
+@admin.register(StatusMessage)
+class StatusMessageAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'message', 'timestamp')
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,4 +9,14 @@ class Profile(models.Model):
     email = models.EmailField()
     profile_image_url = models.URLField(max_length=200)
 
-    
+    def get_status_messages(self):
+        return self.status_messages.all().order_by('-timestamp')
+
+
+class StatusMessage(models.Model):
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='status_messages')
+
+    def __str__(self):
+        return f"Status by {self.profile.first_name} on {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}: {self.message[:20]}..."
