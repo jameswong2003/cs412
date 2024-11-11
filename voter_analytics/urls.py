6f8122d9f484102ffe585ef04ffc972a7b158b7f
James <wong.james2003@gmail.com>
2024-11-11 16:23:14 -0500

    fixes

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -3,6 +3,6 @@ from .views import VoterListView, VoterDetailView, GraphListView
 
 urlpatterns = [
     path('', VoterListView.as_view(), name='voters'),
-    path('/voter/<int:pk>/', VoterDetailView.as_view(), name='voter'),
-    path('/graphs/', GraphListView.as_view(), name='graphs'),
+    path('voter/<int:pk>/', VoterDetailView.as_view(), name='voter'),
+    path('graphs/', GraphListView.as_view(), name='graphs'),
 ]
